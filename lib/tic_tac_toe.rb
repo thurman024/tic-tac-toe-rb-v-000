93592271b15e4b15ef983518a32255eb59e51036
Stephen Thurman <thurman024@gmail.com>
2018-01-17 14:56:33 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,6 +22,22 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your input_to_index and move method here!
+def input_to_index(answer)
+  index = answer.to_i - 1
+end
+
+def move(board, index, team = "X")
+  board[index] = team
+end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
   [0,1,2],
